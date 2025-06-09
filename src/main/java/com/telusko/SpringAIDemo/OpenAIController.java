9ecd9732b59b961eebdb563ca8ceedf290b1ed07
shishal singh <shishal.a@gmail.com>
2025-06-09 17:00:40 +0200

    added open api integration

--- src/main/java/com/telusko/SpringAIDemo/OpenAIController.java
+++ src/main/java/com/telusko/SpringAIDemo/OpenAIController.java
@@ -4,8 +4,8 @@ import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-//@RestController
-//@RequestMapping("/api/openai")
+@RestController
+@RequestMapping("/api/openai")
 @CrossOrigin("*")
 public class OpenAIController {
 
