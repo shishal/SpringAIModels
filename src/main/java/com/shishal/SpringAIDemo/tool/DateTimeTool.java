b15df09f5cd0a3d12f1ecf120f6deaf569d8d4a1
shishal singh <shishal.a@gmail.com>
2025-06-15 01:24:57 +0200

    multi tool

--- src/main/java/com/shishal/SpringAIDemo/tool/DateTimeTool.java
+++ src/main/java/com/shishal/SpringAIDemo/tool/DateTimeTool.java
@@ -3,12 +3,21 @@ package com.shishal.SpringAIDemo.tool;
 import org.springframework.ai.tool.annotation.Tool;
 import org.springframework.context.i18n.LocaleContextHolder;
 
+import java.time.DayOfWeek;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class DateTimeTool
 {
     @Tool(description = "Get the current date and time in the user's timezone")
     String getCurrentDateTime() {
         return LocalDateTime.now().atZone(LocaleContextHolder.getTimeZone().toZoneId()).toString();
+    }
+
+    @Tool(description = "Find out if given date is allowed for work from home or not")
+    boolean isWorkFromHomeAllowed( String localDateTime){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssXXX");
+        LocalDateTime dateTime = LocalDateTime.parse(localDateTime, formatter);
+        return !dateTime.getDayOfWeek().equals(DayOfWeek.TUESDAY);
     }
 }
